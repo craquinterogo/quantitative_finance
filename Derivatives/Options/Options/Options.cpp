b9b617d260286035b6e61d75e19433b462027140
Cristian Arley Quintero Gonzalez <7codev@gmail.com>
2021-01-09 22:30:07 -0500

    Adding of templates to Options

--- Derivatives/Options/Options/Options.cpp
+++ Derivatives/Options/Options/Options.cpp
@@ -13,37 +13,48 @@ struct OptionException : std::exception {
 	}
 };
 
-
-Option::Option(float S, float K, float rf, float sigma, float T, int psi) :
+template<class type_S, class type_K, class type_rf, class type_sigma, class type_T, class type_psi>
+Option<type_S, type_K, type_rf, type_sigma, type_T, type_psi>::Option(type_S S, type_K K, type_rf rf, type_sigma sigma, type_T T, type_psi psi) :
 	S(S), K(K), rf(rf), sigma(sigma), T(T), psi(psi)
 {
 	cout << "Constructor" << endl;
 }
 
-European::European(float S, float K, float rf, float sigma, float T, int psi) : Option(S, K, rf, sigma, T, psi)
+template<class type_S, class type_K, class type_rf, class type_sigma, class type_T, class type_psi>
+European<type_S, type_K, type_rf, type_sigma, type_T, type_psi>::European(type_S S, type_K K, type_rf rf, type_sigma sigma, type_T T, type_psi psi) :
+	Option<type_S, type_K, type_rf, type_sigma, type_T, type_psi>(S, K, rf, sigma, T, psi)
 {
 	d1 = NULL;
 	d2 = NULL;
 };
 
-void European::calculate_d1() {
-	d1 = (float)(log(S / K) + (rf + 0.5 * sigma * sigma) * T) / (sigma * sqrt(T));
+template<class type_S, class type_K, class type_rf, class type_sigma, class type_T, class type_psi>
+void European<type_S, type_K, type_rf, type_sigma, type_T, type_psi>::calculate_d1() {
+	d1 = (float)(log(this->S / this->K) + (this->rf + 0.5 * this->sigma * this->sigma) * this->T) / (this->sigma * sqrt(this->T));
 };
 
-void European::calculate_d2() {
-	d2 = d1 - sigma * sqrt(T);
+template<class type_S, class type_K, class type_rf, class type_sigma, class type_T, class type_psi>
+void European<type_S, type_K, type_rf, type_sigma, type_T, type_psi>::calculate_d2() {
+	d2 = d1 - this->sigma * sqrt(this->T);
 };
 
-double European::valuation() {
+template<class type_S, class type_K, class type_rf, class type_sigma, class type_T, class type_psi>
+double European<type_S, type_K, type_rf, type_sigma, type_T, type_psi>::valuation() {
 	calculate_d1();
 	calculate_d2();
 	normal s;
-	double option_price = ((double)psi * S * cdf(s, psi * d1)) - ((double)psi * K * exp(-rf * T) * cdf(s, psi * d2));
+	double option_price = ((double)this->psi * this->S * cdf(s, this->psi * d1)) - ((double)this->psi * this->K * exp(-this->rf * this->T) * cdf(s, this->psi * this->d2));
 	return option_price;
 };
 
-American::American(float S, float K, float rf, float sigma, float T, int psi) : Option(S, K, rf, sigma, T, psi)
+template<class type_S, class type_K, class type_rf, class type_sigma, class type_T, class type_psi>
+American<type_S, type_K, type_rf, type_sigma, type_T, type_psi>::American(type_S S, type_K K, type_rf rf, type_sigma sigma, type_T T, type_psi psi) :
+	Option<type_S, type_K, type_rf, type_sigma, type_T, type_psi>(S, K, rf, sigma, T, psi)
 {
+};
+
+template<class type_S, class type_K, class type_rf, class type_sigma, class type_T, class type_psi>
+double American<type_S, type_K, type_rf, type_sigma, type_T, type_psi>::valuation(){
 };
 
 int main() {
@@ -59,14 +70,14 @@ int main() {
 
 		cout << "CALL OPTION" << endl;
 		cout << "---------------------" << endl;
-		European eur_option_call = European(S, K, rf, sigma, T, psi);
+		European<double, double, double, double, double, int> eur_option_call = European<double, double, double, double, double, int>(S, K, rf, sigma, T, psi);
 		cout << "Option Value :: " << eur_option_call.valuation() << endl;
 		cout << "======================" << endl;
 		cout << endl;
 
 		cout << "PUT OPTION" << endl;
 		cout << "---------------------" << endl;
-		European eur_option_put = European(S, K, rf, sigma, T, -1 * psi);
+		European<double, double, double, double, double, int> eur_option_put = European<double, double, double, double, double, int>(S, K, rf, sigma, T, -1 * psi);
 		cout << "Option Value :: " << eur_option_put.valuation() << endl;
 		cout << "======================" << endl;
 		cout << endl;
--- Derivatives/Options/Options/Options.h
+++ Derivatives/Options/Options/Options.h
@@ -3,20 +3,26 @@
 /* Classes to pricing Option Derivatives */
 
 /* GENERAL CLASS */
+template<class type_S, class type_K, class type_rf, class type_sigma, class type_T, class type_psi>
 class Option {
 public:
-	Option(float S, float K, float rf, float sigma, float T, int psi);
+	Option(type_S S, type_K K, type_rf rf, type_sigma sigma, type_T T, type_psi psi);
 
 protected:
-	float S, K, rf, T, sigma;
-	int psi;
+	type_S S;
+	type_K K;
+	type_rf rf;
+	type_T T;
+	type_sigma sigma;
+	type_psi psi;
 };
 
 /* EUROPEAN CLASS extended from Option Class*/
-class European : Option
+template<class type_S, class type_K, class type_rf, class type_sigma, class type_T, class type_psi>
+class European : Option<type_S, type_K, type_rf, type_sigma, type_T, type_psi>
 {
 public:
-	European(float S, float K, float rf, float sigma, float T, int psi);
+	European(type_S S, type_K K, type_rf rf, type_sigma sigma, type_T T, type_psi psi);
 	double valuation();
 
 private:
@@ -26,10 +32,11 @@ private:
 };
 
 /* EUROPEAN CLASS extended from Option Class*/
-class American : Option
+template<class type_S, class type_K, class type_rf, class type_sigma, class type_T, class type_psi>
+class American : Option<type_S, type_K, type_rf, type_sigma, type_T, type_psi>
 {
 public:
-	American(float S, float K, float rf, float sigma, float T, int psi);
+	American(type_S S, type_K K, type_rf rf, type_sigma sigma, type_T T, type_psi psi);
 	double valuation();
 };
 
